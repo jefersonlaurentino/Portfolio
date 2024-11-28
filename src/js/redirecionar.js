6a5b4eb5e67fa67d33054109c58de721829a3067
Jeferson Laurentino <128257769+jefersonlaurentino@users.noreply.github.com>
2024-11-28 18:24:36 -0300

    bugs fix

--- src/js/redirecionar.js
+++
@@ -1,6 +0,0 @@
-const url = window.location.href
-if (url.includes('github')) {
-    window.location.href ='https://portfolio-sigma-eight-91.vercel.app/'
-} else {
-    document.querySelector('#url_git').classList.add('hidden')
-}
